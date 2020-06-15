60297133479c977f1578ba05fc1ddf05786e1c4b
leojojo <setsunal.fujita@gmail.com>
2020-06-16 00:34:07 +0900

    scrape pokemon names into list

--- scrape.py
+++ scrape.py
@@ -6,6 +6,7 @@ BASE_URL = 'https://yakkun.com/swsh/zukan/n'
 HEADERS = {'User-Agent': 'Mozilla/5.0 (iPhone; CPU iPhone OS 13_2_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.0.3 Mobile/15E148 Safari/604.1'}
 POKEDEX_MAX = 890
 MOVE_MAX = 804
+param_id = lambda x: int(re.search(r'\d+', x.a.get('href')).group())
 
 def req_soup(poke_num):
     page = requests.get(BASE_URL + str(poke_num), headers=HEADERS)
@@ -15,7 +16,6 @@ def req_soup(poke_num):
 # id = level_move, machine_move, record_move, egg_move
 def get_moves(soup):
     moves = soup.find(id='level_move').ul.findAll('li')
-    param_id = lambda x: int(re.search(r'\d+', x.a.get('href')).group())
     return list(map(param_id, moves))
 
 def get_move_matrix():
@@ -26,8 +26,16 @@ def get_move_matrix():
             matrix[n, move] = 1
     return matrix
 
+def get_poke_list():
+    soup = req_soup('')
+    get_name = lambda li: li.a.get_text()
+    lis = soup.find(class_='pokemon_list').findAll('li')
+    poke_list = dict(zip( map(param_id, lis), map(get_name, lis) ))
+    srtd = sorted(poke_list.items(), key=lambda x:x[0])
+    return [ s[1] for s in srtd ]
+
 def main():
-    print(get_move_matrix())
+    print(get_poke_list())
 
 if __name__ == '__main__':
     main()
